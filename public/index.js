d6214a845098e1b8c63705bf69deb81062c91e8d
Iago <iagodeoliveira.sa@gmail.com>
2023-10-31 17:44:51 -0300

    finished search feature

--- public/index.js
+++ public/index.js
@@ -1,40 +1,68 @@
 // Dom
-const GREEN = "#86efac";
-const DIMMED = "#e2e8f0";
 const _array = document.getElementById("array");
 const _arrayLength = document.getElementById("options__length");
 const _arraySubmit = document.getElementById("options__submit");
-
+const _arrayTarget = document.getElementById("options__input");
+const _arraySearch = document.getElementById("options__search");
 
 class BinarySearch {
-    static search(array, target) {
+    static async search(array, target) {
         let start = 0;
         let end = array.length - 1;
         let middle = Math.round((end - start) / 2);
+        array[middle].setAttribute("isMiddle", true);
 
-        console.log(middle)
-
-        array[middle].style.backgroundColor = GREEN;
-        array[middle].style.boxShadow = `0 0 2.4rem ${GREEN}`;
-
-        for (let i = 0; i < array.length; i++) {
-            setTimeout(() => {
-                if (target == middle) { return "Found it!" }
-                console.log(middle);
-    
-                if (target > middle) {
-                    start = middle;
-                    middle = Math.round((end - start) / 2);
-                }
-                if (target < middle) {
-                    end = middle;
-                    middle = Math.round((end - start) / 2);
-                }
-    
-                if (i >= 10) { return "Passou de 10 tentavias." }
-            }, 1000);
-
+        if (target > end || target < start) {
+            Notification.trigger("Out of range!")
+            return;
         }
+
+        for (let i = 0; i < end; i++) {
+            if (target > middle) {
+                start = middle;
+                array[middle].setAttribute("isMiddle", false);
+                middle = Math.round((start + end) / 2);
+                array[middle].setAttribute("isMiddle", true);
+            } else if (target < middle) {
+                end = middle;
+                array[middle].setAttribute("isMiddle", false);
+                middle = Math.round((start + end) / 2);
+                array[middle].setAttribute("isMiddle", true);
+            } else {
+                array[middle].setAttribute("isMiddle", true);
+                return;
+            }
+            ResultRenderer.render(i + 1)
+
+            // Cria uma promessa setTimeout
+            await Timer.stop(1000)
+
+            console.log(`Target: ${target} | Middle: ${middle} | Start: ${start} | End: ${end}`);
+        };
+    }
+}
+
+class Notification {
+    static trigger(string) {
+        alert(string);
+    }
+}
+
+class Timer {
+    static async stop(ms) {
+        await new Promise(resolve => setTimeout(resolve, ms))
+    }
+}
+
+const ResultRenderer = {
+    render: (i) => {
+        let _currentResult = document.querySelectorAll(".result")
+        if (_currentResult.length >= 1) { _currentResult[0].remove(); }
+
+        const _result = document.createElement("div");
+        _result.classList.add("result");
+        _result.textContent = i;
+        document.body.appendChild(_result);
     }
 }
 
@@ -46,34 +74,27 @@ const ArrayRenderer = {
             _card.setAttribute("content", i);
             _card.textContent = i;
             _array.appendChild(_card);
-            console.log("run")
         }
     },
     flush: async () => {
         let _elements = document.querySelectorAll(".array__element");
         if ( _elements.length == 0 ) { return; }
-         _elements.forEach(element => {
-            setTimeout(() => {
-                element.remove();
-            }, 100);
+        _elements.forEach(element => {
+            element.remove();
         });
     }
 }
 
-_arrayLength.value = 5;
+_arrayLength.value = 10;
+_arrayTarget.value = 5;
 
-_arrayLength.addEventListener("keydown", (e) => {
-    if (e.code != "Enter") { return; }
 
+_arraySubmit.addEventListener("click", (e) => {
     ArrayRenderer.flush();
     ArrayRenderer.render(_arrayLength.value);
-    let _elements = document.querySelectorAll(".array__element");
-    BinarySearch.search(_elements, 5);
 })
 
-_arraySubmit.addEventListener("click", () => {
-    ArrayRenderer.flush();
-    ArrayRenderer.render(_arrayLength.value);
+_arraySearch.addEventListener("click", (e) => {
     let _elements = document.querySelectorAll(".array__element");
-    console.log(BinarySearch.search(_elements, 5));
+    BinarySearch.search(_elements, _arrayTarget.value);
 })
